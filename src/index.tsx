a8766dea963e8b356a9d54ff6c5ea8c57e709d8c
imtiyaj ahamad <imtiyaj.ahamad@tekinspirations.com>
2025-10-25 17:06:54 +0530

    index optimize

--- src/index.tsx
+++ src/index.tsx
@@ -2,27 +2,33 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client/react";
 import App from "./App";
 import theme from "./common/config/theme";
 import client from "./common/config/client";
-import { ApolloProvider } from "@apollo/client/react";
 import { AuthProvider } from "./common/context/AuthContext";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <AuthProvider>
-        <ChakraProvider theme={theme}>
-          {/* Color Mode Script enables light/dark mode preference */}
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-          {/* <BrowserRouter> */}
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-          {/* </BrowserRouter> */}
-        </ChakraProvider>
-      </AuthProvider>
-    </ApolloProvider>
-  </React.StrictMode>
+
+const RootApp = () => (
+  <ApolloProvider client={client}>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </ChakraProvider>
+  </ApolloProvider>
 );
+
+if (process.env.NODE_ENV === "development") {
+  root.render(
+    <React.StrictMode>
+      <RootApp />
+    </React.StrictMode>
+  );
+} else {
+  root.render(<RootApp />);
+}
